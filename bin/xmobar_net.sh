e66476adf78ce7c61b9655bf5d15a919ca78080a
Will Coster <willcoster@gmail.com>
2012-02-24 01:10:56 +0000

    Removed unused vol script. Updated xmobar_net.sh to display vpn status

--- bin/xmobar_net.sh
+++ bin/xmobar_net.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 ethIP=`ifconfig eth0 | sed -rn 's/.*inet addr:([^ ]*).*/\1/gp'`
+pppIP=`ifconfig ppp0 | sed -rn 's/.*inet addr:([^ ]*).*/\1/gp'`
 wlanIP=`ifconfig wlan0 | sed -rn 's/.*inet addr:([^ ]*).*/\1/gp'`
 wlanAP=`iwconfig wlan0 | sed -rn 's/.*ESSID:"([^"]*).*/\1/gp'`
 wlanQ=`iwconfig wlan0 | sed -rn 's/.*Link Quality=([^ ]*).*/\1*100/gp' | bc -l | sed 's/\..*/%/g'`
 
 output=""
+if [ "$pppIP" != "" ]; then
+	output="ppp0: <fc=#00aaff>$pppIP</fc>"
+fi
+
 if [ "$ethIP" != "" ]; then
-	output="eth0: <fc=#00aaff>$ethIP</fc>"
+	if [ "$output" != "" ]; then
+		output=$output", "
+	fi
+	output="$output eth0: <fc=#00aaff>$ethIP</fc>"
 fi
 
 if [ "$wlanIP" != "" ]; then
